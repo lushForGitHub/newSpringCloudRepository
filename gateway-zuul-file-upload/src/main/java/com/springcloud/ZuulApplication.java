dd3d12ffb016297aad5779469c95c2af7124c831
lush <ls75650@foxmail.com>
2018-09-05 10:57:17 +0800

    大文件上传配置

--- gateway-zuul-file-upload/src/main/java/com/springcloud/ZuulApplication.java
+++ gateway-zuul-file-upload/src/main/java/com/springcloud/ZuulApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-// @EnableZuulProxy
+@EnableZuulProxy
 public class ZuulApplication {
 
 	// private int maxUploadSizeMb = 20 * 1024 * 1024;
